4c8d66347924e6e3a36851d76f947942430e43cf
Harsh Aggarwal <harshv.6292@gmail.com>
2019-04-09 16:47:50 -0400

    Add navigation architecture component and dummy fragments.

--- app/src/main/java/com/harshagg/pmrh/locationnotes/ui/activity/NotesMainActivity.kt
+++ app/src/main/java/com/harshagg/pmrh/locationnotes/ui/activity/NotesMainActivity.kt
@@ -1,31 +1,32 @@
 package com.harshagg.pmrh.locationnotes.ui.activity
 
-import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
+import com.harshagg.pmrh.locationnotes.AddNewNoteFragment
+import com.harshagg.pmrh.locationnotes.AllNotesFragment
 import com.harshagg.pmrh.locationnotes.R
 import com.harshagg.pmrh.locationnotes.ui.viewmodel.NotesActivityViewModel
 import com.harshagg.pmrh.locationnotes.utilities.InjectorUtils
 import kotlinx.android.synthetic.main.activity_notes_main.*
 
-class NotesMainActivity : AppCompatActivity() {
+class NotesMainActivity : AppCompatActivity(), AllNotesFragment.OnFragmentInteractionListener, AddNewNoteFragment.OnFragmentInteractionListener {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -50,15 +51,22 @@ class NotesMainActivity : AppCompatActivity() {
 
         mNotesActivityViewModel!!.getAllNotes().observe(this, Observer { noteEntries ->
             if (noteEntries != null) {
-                Snackbar.make(message, "Total Notes found - " + noteEntries.size, Snackbar.LENGTH_SHORT).show()
+                Snackbar.make(addNoteButton, "Total Notes found - " + noteEntries.size, Snackbar.LENGTH_SHORT).show()
             } else {
-                Snackbar.make(message, "No Notes found", Snackbar.LENGTH_SHORT).show()
+                Snackbar.make(addNoteButton, "No Notes found", Snackbar.LENGTH_SHORT).show()
             }
         })
 
-        notes_add_icon.setOnClickListener { view ->
-            intent = Intent(this, NotesDetailActivity::class.java)
-            startActivityForResult(intent, REQUEST_CODE_ADD_NEW_NOTE)
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.notes_nav_host_fragment) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        addNoteButton.setOnClickListener {
+            //findNavController(R.id.notes_nav_host_fragment).navigate(R.id.action_allNotesFragment_to_addNewNoteFragment)
+            navController.navigate(R.id.action_allNotesFragment_to_addNewNoteFragment)
         }
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
